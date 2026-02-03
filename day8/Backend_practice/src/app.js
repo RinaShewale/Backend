5898908aef982a3fbc4a0336d5237c3ab538abf7
rinashewale111 <rinashewale6@gmail.com>
2026-02-03 20:28:13 +0530

    day9

--- day8/Backend_practice/src/app.js
+++ day8/Backend_practice/src/app.js
@@ -2,9 +2,12 @@ const express= require("express")
 const notemodal=require("./config/modal/note.modal")
 const cors=require("cors")
 const app= express()
+const path = require('path')
+
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static("./public"))
 
 app.post("/api/notes", async (req,res)=>{
     const{title,description}=req.body
@@ -42,14 +45,19 @@ app.get("/api/notes", async(req,res)=>{
  app.patch("/api/notes/:id", async (req,res)=>{
     const id = req.params.id;
     const { description } = req.body;
+    const {title}=req.body
     
-    const note = await notemodal.findByIdAndUpdate(id, { description }); 
+    const note = await notemodal.findByIdAndUpdate(id, { description ,title}); 
 
      res.status(200).json({
         message:"note is modified",
         note
     })
  })
- 
 
+app.use("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+});
+
+ 
 module.exports=app
--- day8/Frontend_practice/frontend_practice/src/App.jsx
+++ day8/Frontend_practice/frontend_practice/src/App.jsx
@@ -1,32 +1,115 @@
 import { useState } from 'react'
 import axios from 'axios'
+import { useEffect } from 'react'
 
 
 
 function App() {
- const [Notes, setNotes] = useState([])
+  const [Notes, setNotes] = useState([])
 
- axios.get("http://localhost:3000/api/notes")
-  .then((res)=>{
-    console.log("res.data", res.data);
-    setNotes(res.data.notes); 
-  })
- 
+  console.log("hellowww");
 
-  return (
-    <>
 
-  <div className="notes">
-    {Notes.map((note) => (
-      <div className="note" key={note.title}>
-        <h1>{note.title}</h1>
-        <p>{note.description}</p>
-      </div>
-    ))}
-  </div>
+  function fetchNotes() {
+    axios.get("http://localhost:3000/api/notes")
+      .then((res) => {
+        console.log("res.data", res.data);
+        setNotes(res.data.notes);
+      })
+  }
 
-    </>
-  )
+
+  useEffect(() => {
+
+    fetchNotes();
+  }, [])
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const { title, description } = e.target.elements
+    console.log("title", title.value);
+    console.log("description", description.value);
+
+    axios.post("http://localhost:3000/api/notes", {
+      title: title.value,
+      description: description.value
+    })
+      .then(res => {
+        fetchNotes();
+      })
+  }
+
+  function handledelete(note_id) {
+    axios.delete(`http://localhost:3000/api/notes/${note_id}`)
+      .then(res => {
+        fetchNotes();
+      })
+  }
+
+
+  function handleUpdate(note) {
+    const newtitle = prompt("enter new title", note.title);
+    const newdescription = prompt("enter new description", note.description);
+    axios.patch(`http://localhost:3000/api/notes/${note._id}`, {
+      title: newtitle,
+      description: newdescription
+    })
+      .then(res => {
+        fetchNotes();
+      })
+  }
+return (
+  <>
+    <form
+      className="note_created_form"
+      onSubmit={handleSubmit}
+      autoComplete="off"
+    >
+      <input
+        type="text"
+        name="title"
+        placeholder="Title"
+      />
+
+      <input
+        type="text"
+        name="description"
+        placeholder="Description"
+      />
+
+      <button type="submit">
+        Create Note
+      </button>
+    </form>
+
+    <div className="notes">
+      {Notes.map((note) => (
+        <div className="note" key={note._id}>
+          <h1>{note.title}</h1>
+          <p>{note.description}</p>
+
+          <div className="buttons">
+            <button
+              className="delete"
+              onClick={() => handledelete(note._id)}
+            >
+              Delete
+            </button>
+
+            <button
+              className="update"
+              onClick={() => handleUpdate(note)}
+            >
+              Update
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  </>
+);
+
 }
 
 export default App
